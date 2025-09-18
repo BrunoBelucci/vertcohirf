288460bc3af8075fea7c753ab487b8e61e42baeb
Bruno Belucci <bruno.belucci-teixeira@dauphine.eu>
2025-09-18 17:06:37 +0200

    implement distributed kmeans

--- cocohirf/models/distributed_kmeans.py
+++ cocohirf/models/distributed_kmeans.py
@@ -5,12 +5,10 @@ from sklearn.cluster import KMeans
 from joblib import Parallel, delayed
 
 
-class CoresetKMeans(ClusterMixin, BaseEstimator):
+class DistributedKMeans(ClusterMixin, BaseEstimator):
 
     def __init__(
         self,
-        coreset_size_div=10,
-        alpha=2,
         n_jobs=1,
         random_state=None,
         # kmeans parameters
@@ -20,8 +18,6 @@ class CoresetKMeans(ClusterMixin, BaseEstimator):
         kmeans_max_iter: int = 300,
         kmeans_tol: float = 1e-4,
     ):
-        self.coreset_size_div = coreset_size_div
-        self.alpha = alpha
         self.n_jobs = n_jobs
         self._random_state = random_state
         self.kmeans_n_clusters = kmeans_n_clusters
@@ -56,7 +52,8 @@ class CoresetKMeans(ClusterMixin, BaseEstimator):
         distances_from_centers = kmeans.transform(X_group)
         # Select the distance from the closest center for each sample
         distances_from_cluster_centers = distances_from_centers[np.arange(X_group.shape[0]), labels]
-        return labels, cost, distances_from_cluster_centers
+        cluster_centers = kmeans.cluster_centers_
+        return labels, cost, distances_from_cluster_centers, cluster_centers
 
     def fit( # type: ignore
         self,
@@ -65,53 +62,28 @@ class CoresetKMeans(ClusterMixin, BaseEstimator):
         y=None,
         sample_weight=None,
     ):
+        n_samples = X.shape[0]
+        sample_weight = np.ones(n_samples) / n_samples if sample_weight is None else sample_weight
         self.features_groups = features_groups
         n_agents = len(features_groups)
-        n_samples = X.shape[0]
-        coreset_size = n_samples // self.coreset_size_div 
         results_i = Parallel(n_jobs=self.n_jobs)(delayed(self.run_local_kmeans)(X, r) for r in range(n_agents))
-        labels_i, costs_i, distances_i = zip(*results_i)
-        sensitivity = np.zeros((n_samples, n_agents))
-        for i in range(n_agents):
-            group_count = np.zeros(self.kmeans_n_clusters)
-            group_cost = np.zeros(self.kmeans_n_clusters)
-            group_count = np.bincount(labels_i[i], minlength=self.kmeans_n_clusters)
-            group_cost = np.bincount(labels_i[i], weights=distances_i[i]**2, minlength=self.kmeans_n_clusters)
-            # Safe division to avoid division by zero
+        labels_i, costs_i, distances_i, cluster_centers_i = zip(*results_i)
 
-            # Precompute denominators
-            denom1 = costs_i[i]
-            denom2 = group_count[labels_i[i]] * costs_i[i]
-            denom3 = group_count[labels_i[i]]
+        # Vectorized center_list construction
+        # Get all possible combinations of cluster indices for n_agents
+        all_indices = np.array(np.meshgrid(*[np.arange(self.kmeans_n_clusters)] * n_agents)).T.reshape(-1, n_agents)
+        # For each agent, select the cluster centers for all combinations
+        center_parts = [
+            cluster_centers_i[j][all_indices[:, j], :] for j in range(n_agents)
+        ]
+        # Concatenate along feature axis
+        center_list = np.concatenate(center_parts, axis=1)
 
-            # Safe division for each term
-            term1 = np.divide(
-                self.alpha * (distances_i[i] ** 2),
-                denom1,
-                out=np.zeros_like(distances_i[i], dtype=float),
-                where=denom1 != 0
-            )
-            term2 = np.divide(
-                2 * self.alpha * group_cost[labels_i[i]],
-                denom2,
-                out=np.zeros_like(distances_i[i], dtype=float),
-                where=denom2 != 0
-            )
-            term3 = np.divide(
-                4 * n_samples,
-                denom3,
-                out=np.zeros_like(distances_i[i], dtype=float),
-                where=denom3 != 0
-            )
-            sensitivity[:, i] = term1 + term2 + term3
+        labels_matrix = np.vstack(labels_i)
+        indices = np.sum(labels_matrix[::-1, :] * (self.kmeans_n_clusters ** np.arange(n_agents)[:, None]), axis=0)
+        # Use bincount to sum sample_weight for each unique index
+        center_weights = np.bincount(indices, weights=sample_weight, minlength=center_list.shape[0])
 
-        sensitivity_sum = np.sum(sensitivity, axis=1)
-        weights = sensitivity_sum / sensitivity_sum.sum()
-        indices = self.random_state.choice(len(sensitivity_sum), size=coreset_size, replace=False, p=weights)
-        C = np.hstack((X[indices], (1 / sensitivity_sum[indices]).reshape(-1, 1)))
-        data = C[:, :-1]
-        weights = C[:, -1]
-        weights = weights / np.sum(weights)
         server_kmeans = KMeans(
             n_clusters=self.kmeans_n_clusters,
             init=self.kmeans_init,
@@ -120,8 +92,9 @@ class CoresetKMeans(ClusterMixin, BaseEstimator):
             tol=self.kmeans_tol,
             random_state=self.random_state.integers(0, 1e6),
         )
-        server_kmeans.fit(data, sample_weight=weights)
+        server_kmeans.fit(center_list, sample_weight=center_weights)
         server_kmeans_clusters_centers = server_kmeans.cluster_centers_
+
         labels = []
         costs = []
         for i in range(n_agents):
@@ -138,7 +111,7 @@ class CoresetKMeans(ClusterMixin, BaseEstimator):
         self.labels_ = labels
         self.costs_ = costs
 
-    def fit_predict( # type: ignore
+    def fit_predict(  # type: ignore
         self,
         X: pd.DataFrame | np.ndarray,
         features_groups: list[list[int]],
