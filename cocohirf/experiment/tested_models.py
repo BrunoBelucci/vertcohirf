f71bd53261026fcac4c24e27db1a1cb148050a19
Bruno Belucci <bruno.belucci-teixeira@dauphine.eu>
2025-12-06 15:16:39 +0100

    fix tested_models

--- cocohirf/experiment/tested_models.py
+++ cocohirf/experiment/tested_models.py
@@ -9,6 +9,7 @@ from cocohirf.models.coreset_kmeans import CoresetKMeans
 from cocohirf.models.distributed_kmeans import DistributedKMeans
 from cocohirf.models.dpvfl import DPVFL
 from ml_experiments.utils import update_recursively
+from copy import deepcopy
 
 
 models_dict = {
@@ -82,10 +83,10 @@ two_stage_models_dict = dict()
 cohirf_models = [model_name for model_name in models_dict.keys() if model_name.startswith("CoHiRF")]
 for model_name in cohirf_models:
     model, model_params, search_space, default_values = models_dict[model_name]
-    model_1 = model
-    model_params_1 = model_params
-    search_space_1 = search_space
-    default_values_1 = default_values
+    model_1 = deepcopy(model)
+    model_params_1 = deepcopy(model_params)
+    search_space_1 = deepcopy(search_space)
+    default_values_1 = deepcopy(default_values)
     if model_name == CoHiRF.__name__:
         vecohirf_name = VeCoHiRF.__name__
     else:
@@ -109,14 +110,14 @@ for model_name in cohirf_models:
 # create 1-iteration VeCoHiRF versions of VeCoHiRF models
 vecohirf_models = [model_name for model_name in two_stage_models_dict.keys() if model_name.startswith(VeCoHiRF.__name__)]
 for model_name in vecohirf_models:
-    model_1 = two_stage_models_dict[model_name]["model_1"]
-    model_params_1 = two_stage_models_dict[model_name]["model_params_1"]
-    search_space_1 = two_stage_models_dict[model_name]["search_space_1"]
-    default_values_1 = two_stage_models_dict[model_name]["default_values_1"]
-    model_2 = two_stage_models_dict[model_name]["model_2"]
-    model_params_2 = two_stage_models_dict[model_name]["model_params_2"]
-    search_space_2 = two_stage_models_dict[model_name]["search_space_2"]
-    default_values_2 = two_stage_models_dict[model_name]["default_values_2"]
+    model_1 = deepcopy(two_stage_models_dict[model_name]["model_1"])
+    model_params_1 = deepcopy(two_stage_models_dict[model_name]["model_params_1"])
+    search_space_1 = deepcopy(two_stage_models_dict[model_name]["search_space_1"])
+    default_values_1 = deepcopy(two_stage_models_dict[model_name]["default_values_1"])
+    model_2 = deepcopy(two_stage_models_dict[model_name]["model_2"])
+    model_params_2 = deepcopy(two_stage_models_dict[model_name]["model_params_2"])
+    search_space_2 = deepcopy(two_stage_models_dict[model_name]["search_space_2"])
+    default_values_2 = deepcopy(two_stage_models_dict[model_name]["default_values_2"])
     model_params_1 = update_recursively(model_params_1, dict(max_iter=1))
     model_params_2 = update_recursively(model_params_2, dict(cohirf_kwargs_shared=dict(max_iter=1)))
     two_stage_models_dict[model_name + "-1iter"] = dict(
